27dc013a3e93a716b22c2795f763b76c05cd7651
river <riverytacc11@gmail.com>
2025-11-29 04:38:55 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -911,7 +911,7 @@ function createRichestPlayersSection(data) {
   const intro = `
     <div class="richest-players-header">
       <h2>Top 1000 Richest Players in BlockSpin</h2>
-      <p class="richest-intro">This list is the Offical BlockSpin leaderboard showing the wealthiest players in the game, Ranked by the total value of their in-game assets. Rankings go to #1000. This leaderboard updates hourly. Note this section is in BETA, the player record formation may appear weird, we are working on fixes/</p>
+      <p class="richest-intro">This list is the Offical BlockSpin leaderboard showing the wealthiest players in the game, Ranked by the total value of their in-game assets. Rankings go to #1000. This leaderboard updates hourly. Note this section is in BETA, the player record formation may appear weird, we are working on fixes.</p>
       
       <input 
         type="text" 
